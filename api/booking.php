7bffa450e74e49b3f093e3e19aa52a97247a081b
cable <cable870526@gmail.com>
2024-01-19 10:17:48 +0800

    增加無人座位及座位勾選

--- api/booking.php
+++ api/booking.php
@@ -23,6 +23,12 @@ $session=$_GET['session'];
     .seat{
         width: 63px;
         height: 85px;
+        position:relative;
+    }
+    .chk{
+        position: absolute;
+        right: 2px;
+        bottom: 2px;
     }
 </style>
 <div id="room">
@@ -35,6 +41,10 @@ $session=$_GET['session'];
             echo (floor($i/5)+1)."排";
             echo (($i%5)+1) . "號";
             echo "</div>";
+            echo "<div class='ct'>";
+            echo "<img src='./icon/03D02.png'>";
+            echo "</div>";
+            echo "<input type='checkbox' name=chk value='$i' class='chk'>";
             echo "</div>";
         }
         ?>
