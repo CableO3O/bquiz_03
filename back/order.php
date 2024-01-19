fd5ab069549254221ec1445a33b38e1ea9034b48
cable <cable870526@gmail.com>
2024-01-19 14:21:11 +0800

    建立快速刪除功能區

--- back/order.php
+++ back/order.php
@@ -1,7 +1,7 @@
 <h2 class="ct">訂單清單</h2>
 <div class="qdel">
     快速刪除:
-    <input type="radio" name="type" value="1">依日期:
+    <input type="radio" name="type" value="1" checked>依日期:
     <input type="text" name="date" id="date">
     <input type="radio" name="type" value="2">依電影:
     <select name="movie" id="movie">
