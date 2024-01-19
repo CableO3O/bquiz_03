9460f537191fc02335bfcdd3fcff219f58783aac
cable <cable870526@gmail.com>
2024-01-19 13:55:39 +0800

    訂票系統會顯示已訂票圖片並不能再次訂票

--- api/booking.php
+++ api/booking.php
@@ -3,7 +3,16 @@ include_once "db.php";
 $movie = $Movie->find($_GET['movie_id']);
 $date = $_GET['date'];
 $session = $_GET['session'];
-
+$ords = $Order->all([
+    'movie' => $movie['name'],
+    'date' => $date,
+    'session' => $session
+]);
+$seats = [];
+foreach ($ords as $ord) {
+    $tmp = unserialize($ord['seats']);
+    $seats = array_merge($seats, $tmp);
+}
 ?>
 <style>
     #room {
@@ -45,10 +54,16 @@ $session = $_GET['session'];
             echo (($i % 5) + 1) . "號";
             echo "</div>";
             echo "<div class='ct'>";
-            echo "<img src='./icon/03D02.png'>";
+            if (in_array($i, $seats)) {
+                echo "<img src='./icon/03D03.png'>";
+            } else {
+                echo "<img src='./icon/03D02.png'>";
+            }
             echo "</div>";
-            echo "<input type='checkbox' name=chk value='$i' class='chk'>";
-            echo "</div>";
+            if (!in_array($i, $seats)) {
+                echo "<input type='checkbox' name=chk value='$i' class='chk'>";
+                echo "</div>";
+            }
         }
         ?>
     </div>
@@ -67,24 +82,27 @@ $session = $_GET['session'];
     let seats = new Array();
     $(".chk").on("change", function() {
         if ($(this).prop('checked')) {
-            if ((seats.length+1)<=4) {
+            if ((seats.length + 1) <= 4) {
                 seats.push($(this).val());
-            }else{
-                $(this).prop('checked',false);
+            } else {
+                $(this).prop('checked', false);
                 alert("每個人只能勾選四張票");
             }
         } else {
-            seats.splice(seats.indexOf($(this).val()),1)
+            seats.splice(seats.indexOf($(this).val()), 1)
         }
         $("#tickets").text(seats.length)
     })
-function checkout(){
-    $.post("./api/checkout.php",{movie:'<?= $movie['name']; ?>',
-                                 date:'<?= $date; ?>',
-                                 session:'<?= $session; ?>',
-                                 qt:seats.length,
-                                 seats},(no)=>{
-                                    location.href=`?do=result&no=${no}`;
-                                 })
-}
-</script> 
+
+    function checkout() {
+        $.post("./api/checkout.php", {
+            movie: '<?= $movie['name']; ?>',
+            date: '<?= $date; ?>',
+            session: '<?= $session; ?>',
+            qt: seats.length,
+            seats
+        }, (no) => {
+            location.href = `?do=result&no=${no}`;
+        })
+    }
+</script>
